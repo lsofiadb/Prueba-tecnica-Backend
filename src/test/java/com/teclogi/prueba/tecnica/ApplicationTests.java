9186fc888e494b5d008eff4ff0cf3f67e7d3b73a
Laura Sofia Dueñas Bulla <lauraduenas1105@gmail.com>
2022-01-27 19:09:57 -0500

    Initial configuration

---
+++ src/test/java/com/teclogi/prueba/tecnica/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teclogi.prueba.tecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
