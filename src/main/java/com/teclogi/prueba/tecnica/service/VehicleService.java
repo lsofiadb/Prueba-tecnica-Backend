802cb908065a3f1b43b49fc57133f001ddde7c89
Laura Sofia Dueñas Bulla <lauraduenas1105@gmail.com>
2022-01-31 23:53:34 -0500

    Finished

--- src/main/java/com/teclogi/prueba/tecnica/service/VehicleService.java
+++ src/main/java/com/teclogi/prueba/tecnica/service/VehicleService.java
@@ -25,21 +25,24 @@ public class VehicleService {
         double b = 2 * (y2 - y1);
         double c = 2 * (x3 - x2);
         double d = 2 * (y3 - y2);
-        double e = (double) (Math.pow(r1, 2) - Math.pow(r2, 2) - Math.pow(x1, 2) + Math.pow(x2, 2) - Math.pow(y1, 2) + Math.pow(y2, 2));
-        double f = (double) (Math.pow(r2, 2) - Math.pow(r3, 2) - Math.pow(x2, 2) + Math.pow(x3, 2) - Math.pow(y2, 2) + Math.pow(y3, 2));
+        double e = (Math.pow(r1, 2) - Math.pow(r2, 2) - Math.pow(x1, 2) + Math.pow(x2, 2) - Math.pow(y1, 2) + Math.pow(y2, 2));
+        double f = (Math.pow(r2, 2) - Math.pow(r3, 2) - Math.pow(x2, 2) + Math.pow(x3, 2) - Math.pow(y2, 2) + Math.pow(y3, 2));
+
+        /*-------DENOMINATOR!=0------*/
         if(Math.abs((a*d)-(b*c)) >=Math.pow(10,-4)){
             double x = ((e * d) - (b * f)) / ((a * d) - (b * c));
             double y = ((a * f) - (e * c)) / ((a * d) - (b * c));
-            double equation1 = (double) (Math.pow((x - x1), 2) + Math.pow((y - y1), 2));
-            double equation2 = (double) (Math.pow((x - x2), 2) + Math.pow((y - y2), 2));
-            double equation3 = (double) (Math.pow((x - x3), 2) + Math.pow((y - y3), 2));
-            double equality1 = (double) Math.pow(r1, 2) - equation1;
-            double equality2 = (double) Math.pow(r2, 2) - equation2;
-            double equality3 = (double) Math.pow(r3, 2) - equation3;
+            double equation1 = (Math.pow((x - x1), 2) + Math.pow((y - y1), 2));
+            double equation2 = (Math.pow((x - x2), 2) + Math.pow((y - y2), 2));
+            double equation3 = (Math.pow((x - x3), 2) + Math.pow((y - y3), 2));
+            double equality1 = Math.pow(r1, 2) - equation1;
+            double equality2 = Math.pow(r2, 2) - equation2;
+            double equality3 = Math.pow(r3, 2) - equation3;
 
+            /*--------ERROR RANGE--------*/
             if (Math.abs(equality1) < Math.pow(10, -2) & Math.abs(equality2) < Math.pow(10, -2) & Math.abs(equality3) < Math.pow(10, -2)) {
-                vehiclePosition.setX(roundNumber((double) x, 1));
-                vehiclePosition.setY(roundNumber((double) y, 1));
+                vehiclePosition.setX(roundNumber(x, 1));
+                vehiclePosition.setY(roundNumber(y, 1));
                 return vehiclePosition;
             } else {
                 return null;
@@ -48,11 +51,11 @@ public class VehicleService {
             return null;
         }
     }
-
+    /*----FOR OUTPUT----*/
     public double roundNumber(double number, int digitsNumber) {
-        double result = number * (double) Math.pow(10, digitsNumber);
+        double result = number *  Math.pow(10, digitsNumber);
         result = Math.round(result);
-        result = result / (double) Math.pow(10, digitsNumber);
+        result = result / Math.pow(10, digitsNumber);
         return result;
     }
 
@@ -61,13 +64,13 @@ public class VehicleService {
         int sequences = 0;
         char[][] matrix = new char[messages.length][messages.length];
 
-        System.out.println("MATRIX");
+      //  System.out.println("MATRIX");
         for (int i = 0; i < messages.length; i++) {
             for (int j = 0; j < messages.length; j++) {
                 matrix[i][j] = messages[i].charAt(j);
-                System.out.print(matrix[i][j] + " ");
+              //  System.out.print(matrix[i][j] + " ");
             }
-            System.out.println("");
+          //  System.out.println("");
         }
 
         /*-----PATH 1: HORIZONTAL -------*/
@@ -114,8 +117,8 @@ public class VehicleService {
             }
         }
 
-        System.out.println("sequences");
-        System.out.println(sequences);
+       // System.out.println("sequences");
+       // System.out.println(sequences);
 
         if (sequences > 1) {
             return true;
